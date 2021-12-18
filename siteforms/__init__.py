11d1fcf48643d5d5e1451a5e9d45d3464f52f77b
idle sign <idlesign@yandex.ru>
2021-12-18 12:55:30 +0700

    config.py -> apps.py.

--- siteforms/__init__.py
+++ siteforms/__init__.py
@@ -7,4 +7,4 @@ VERSION_STR = '.'.join(map(str, VERSION))
 """Application version number string."""
 
 
-default_app_config = 'siteforms.config.SiteformsConfig'
+default_app_config = 'siteforms.apps.SiteformsConfig'
